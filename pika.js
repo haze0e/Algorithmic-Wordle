f48885f9188bfafb1d3acc55cfb3164ea4f49de7
haze <chiteshjindal2008@gmail.com>
2026-04-01 00:14:15 +0530

    log screen added

--- pika.js
+++ pika.js
@@ -1,7 +1,7 @@
 const board = document.getElementById("board");
 const rows = 6;
 const cols = 5;
-
+const restartBtn = document.getElementById("restart-btn");
 
 
 const words = [
@@ -240,6 +240,17 @@ const words = [
 ];
 
 
+const terminalContent = document.getElementById("terminal-content");
+
+
+function logToTerminal(message) {
+    const newLog = document.createElement("div");
+    newLog.innerText = `> ${message}`;
+    terminalContent.appendChild(newLog);
+
+
+    terminalContent.scrollTop = terminalContent.scrollHeight;
+}
 
 function contain(s, c) {
     let exist = false;
@@ -281,12 +292,26 @@ function contain_not_at_pos(s, c, pos) {
     return exist;
 }
 
+let currentRow = 0;
+let currentCol = 0;
+let Random_word = "";
+let isGameOver = false;
+let isProcessing = false;
+
 function start_game() {
-    let currentRow = 0;
-    let currentCol = 0;
+    terminalContent.innerHTML = "";
+    currentRow = 0;
+    currentCol = 0;
+    isGameOver = false;
+    isProcessing = false;
 
     const randomIndex = Math.floor(Math.random() * words.length);
-    let Random_word = words[randomIndex];
+    Random_word = words[randomIndex];
+    console.log("Target Word choosen");
+    logToTerminal("A random word choosen :)")
+
+
+    board.innerHTML = "";
 
     for (let i = 0; i < rows * cols; i++) {
         let tile = document.createElement("div");
@@ -294,114 +319,141 @@ function start_game() {
         tile.setAttribute("id", `tile-${i}`);
         board.appendChild(tile);
     }
+    if (restartBtn) {
+        restartBtn.blur();
+    }
+}
+
+document.addEventListener("keyup", (e) => {
+    if (isGameOver || isProcessing || currentRow >= rows) return;
+    const pressedKey = e.key.toLowerCase();
 
 
-    document.addEventListener("keyup", (e) => {
+    if (pressedKey >= "a" && pressedKey <= "z" && pressedKey.length === 1) {
+        if (currentCol < cols) {
+            const tileIndex = currentRow * cols + currentCol;
+            const tile = document.getElementById(`tile-${tileIndex}`);
+            tile.classList.add("animate-pop");
+            
+           
+            tile.innerText = pressedKey; 
+            
+            currentCol++;
+            setTimeout(() => {
+                tile.classList.remove("animate-pop");
+            }, 100);
+        }
+    }
 
-        if (currentRow >= rows) return;
+    else if (e.key === "Backspace") {
+        if (currentCol > 0) {
+            currentCol--;
+            const tileIndex = currentRow * cols + currentCol;
+            const tile = document.getElementById(`tile-${tileIndex}`);
+            tile.innerText = "";
+        }
+    }
+    else if (e.key === "Enter") {
+        if (currentCol === cols) {
 
-        if (e.key >= "a" && e.key <= "z" && e.key.length === 1) {
-            if (currentCol < cols) {
+            isProcessing = true;
 
-                const tileIndex = currentRow * cols + currentCol;
+            let user_string = "";
+
+            for (let i = 0; i < cols; i++) {
+                const tileIndex = (currentRow * cols) + i;
                 const tile = document.getElementById(`tile-${tileIndex}`);
-                tile.classList.add("animate-pop");
-                tile.innerText = e.key;
-                currentCol++;
+                user_string += tile.innerText.toLowerCase();
+            }
+
+
+            let code = "";
+
+            for (let j = 0; j < cols; j++) {
+                let charac = user_string[j];
+                const tileIndex = (currentRow * cols) + j;
+                const tile = document.getElementById(`tile-${tileIndex}`);
+
+                let tileColor = "";
+                if (contain_at_pos(Random_word, charac, j + 1)) {
+                    code += "g" + charac + (j + 1);
+                    tileColor = "green";
+                } else if (contain(Random_word, charac)) {
+                    code += "y" + charac + (j + 1);
+                    tileColor = "yellow";
+                } else {
+                    code += "b" + charac + (j + 1);
+                    tileColor = "grey";
+                }
+
+                const flipDelay = j * 250;
+
                 setTimeout(() => {
-                    tile.classList.remove("animate-pop");
-                }, 100);
-            }
-        }
-
-
-        else if (e.key === "Backspace") {
-            if (currentCol > 0) {
-                currentCol--;
-                const tileIndex = currentRow * cols + currentCol;
-                const tile = document.getElementById(`tile-${tileIndex}`);
-                tile.innerText = "";
-            }
-        }
-        else if (e.key === "Enter") {
-
-            if (currentCol === cols) {
-                let user_string = "";
-
-
-                for (let i = 0; i < cols; i++) {
-
-                    const tileIndex = (currentRow * cols) + i;
-                    const tile = document.getElementById(`tile-${tileIndex}`);
-                    user_string += tile.innerText.toLowerCase();
-                }
-
-                let code = "";
-
-
-                for (let j = 0; j < cols; j++) {
-                    let charac = user_string[j];
-                    const tileIndex = (currentRow * cols) + j;
-                    const tile = document.getElementById(`tile-${tileIndex}`);
-
-
-                    let tileColor = "";
-                    if (contain_at_pos(Random_word, charac, j + 1)) {
-                        code += "g" + charac + (j + 1);
-                        tileColor = "green";
-                    } else if (contain(Random_word, charac)) {
-                        code += "y" + charac + (j + 1);
-                        tileColor = "yellow";
-                    } else {
-                        code += "b" + charac + (j + 1);
-                        tileColor = "grey";
-                    }
-
-
-                    const flipDelay = j * 250;
-
+                    tile.classList.add("flip");
                     setTimeout(() => {
+                        tile.classList.add(tileColor);
+                    }, 250);
+                }, flipDelay);
+            }
 
-                        tile.classList.add("flip");
 
-                        setTimeout(() => {
-                            tile.classList.add(tileColor);
-                        }, 250);
 
-                    }, flipDelay);
+
+
+
+            let CORRECT = true;
+            for (let t = 0; t < code.length; t += 3) {
+                if (code[t] !== 'g') {
+                    CORRECT = false;
+                    break;
                 }
-                console.log(code);
+            }
 
 
-                let CORRECT = true;
-                for (let t = 0; t < code.length; t += 3) {
-                    if (code[t] !== 'g') {
-                        CORRECT = false;
-                        break;
-                    }
-                }
+            if (!CORRECT && (currentRow<rows)) {
 
+                setTimeout(() => {
+                    logToTerminal(`Input succesfully submitted: ${user_string}`)
+                }, 6 * 250);
+
+                setTimeout(() => {
+                    logToTerminal(`Checker code: ${code}`)
+                }, (6 * 250 + 125));
+            }
+
+
+            setTimeout(() => {
                 if (CORRECT) {
+                    logToTerminal("");
+                    logToTerminal("");
                     alert("You guessed the word! It was: " + Random_word);
+                    logToTerminal("You guessed the word! It was: " + Random_word);
+                    logToTerminal("Win +=1 :)")
+                    logToTerminal("");
+                    logToTerminal("");
 
+                    isGameOver = true;
+                    isProcessing = false;
                     return;
                 }
 
                 currentRow++;
                 currentCol = 0;
+                isProcessing = false;
 
                 if (currentRow >= rows && !CORRECT) {
                     alert("Game Over! The word was: " + Random_word);
+                    logToTerminal("Game Over! The word was: " + Random_word);
+                    logToTerminal("Streak broken :(")
+                    isGameOver = true;
                 }
-            }
+            }, cols * 250);
         }
-    })
-};
-
+    }
+});
 
 start_game();
 
-function restart_game(){
-    board.innerHTML = "";
-    start_game();
+if (restartBtn) {
+    restartBtn.addEventListener("click", start_game);
 }
